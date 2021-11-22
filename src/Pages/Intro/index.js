e6786fde92076d8b0ecbf68964f0de543e6465fb
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-22 16:19:00 -0300

    Remove Intro Page

--- src/Pages/Intro/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './style.css'
-
-export default function Intro(){
-    return(
-        <h1>Intro</h1>
-    )
-}
--- src/Rotas/index.js
+++ src/Rotas/index.js
@@ -7,15 +7,14 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from '../Pages/Home'
 import Erro from '../Pages/Erro'
 import Log from '../Pages/Log'
-import Intro from '../Pages/Intro'
+
 
 export default function Rotas(){
     return(
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Intro/>}/>
                 <Route path='/log' element={<Log/>}/>
-                <Route path='/home' element={<Home/>}></Route>
+                <Route path='/' element={<Home/>}></Route>
                 <Route path='*' element={<Erro/>}/>
             </Routes>
         </BrowserRouter>

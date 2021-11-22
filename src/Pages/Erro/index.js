8ff82d75298f67782ef971b499c8025cce6fe22f
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-22 19:18:07 -0300

    Add UI And Functions

--- src/Pages/Erro/index.js
+++ src/Pages/Erro/index.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './style.css'
 
+// React Router
+import { useLocation, useNavigate } from 'react-router'
+
 export default function Erro(){
+
+    // Get Location And History
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    // Set Src Diretory Images
+    const [srcDiretory, setSrcDirectory] = useState('')
+    useEffect(()=>{
+        if(location.pathname != null){
+            const listSplit = location.pathname.split('/')
+            let x = 0
+            setSrcDirectory('')
+            while(x <= listSplit.length-2){
+                if(x !== listSplit.lenght-2){
+                    setSrcDirectory( s =>  s +"../" )
+                }
+                x++
+            }        
+        }   
+    },[location.pathname])
+
     return(
-        <div>
-            <h1>Erro 404</h1>
-            <h2>Page not found</h2>        
+        <div className='container-erro'>
+            <div className='container-erro-header'>
+                <img onClick={()=>{navigate('/')}} src={srcDiretory + '/assets/logo.png'} alt='Logo Icon'/>
+            </div>      
+            <div className='content-erro'>
+                <h1>404 Erro</h1>
+                <h2>Page not exist</h2>
+
+                <button onClick={()=>{navigate('/')}}>Back to the Home Page</button>
+            </div>      
         </div>
     )
 }

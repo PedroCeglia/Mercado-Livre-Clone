53d872a822b9bd37a85dec953d47b623dc6e51c6
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-22 17:14:15 -0300

    Add Log UI

--- src/Pages/Log/index.js
+++ src/Pages/Log/index.js
@@ -3,6 +3,25 @@ import './style.css'
 
 export default function Log(){
     return(
-        <h1>Cadastro/Login</h1>
+        <div className='container-log'>
+            <div className='container-header-log'>
+                <img src='../assets/logo.png'/>
+            </div>
+            <div className='content-log'>
+                <div className='container-sing-in'>
+                    <h2>Sing-In</h2>
+                    <input type='text' placeholder='ex ...@gmail' id='id-input-nome-sing-in'/>
+                    <input type='email' placeholder='ex ...@gmail' id='id-input-email-sing-in'/>
+                    <input type='password' placeholder='ex ...@gmail' id='id-input-senha-sing-in'/>
+                    <button>Sing-In</button>
+                </div>
+                <div className='log-in-container none'>
+                    <h2>Log-In</h2>
+                    <input type='email' placeholder='ex ...@gmail' id='id-input-email-sing-in'/>
+                    <input type='password' placeholder='ex ...@gmail' id='id-input-senha-sing-in'/>
+                    <button>Sing-In</button>
+                </div>
+            </div>
+        </div>
     )
 }

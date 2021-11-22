3bbce05419aaea52bc7193cffcedcf0e72535af2
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-22 16:08:02 -0300

    Add 404 Erro Page

--- src/Rotas/index.js
+++ src/Rotas/index.js
@@ -8,6 +8,7 @@ export default function Rotas(){
                 <Route path='/' element={<h1>Intro</h1>}/>
                 <Route path='/log' element={<h1>Cadastro/Login</h1>}/>
                 <Route path='/home' element={<h1>Home</h1>}></Route>
+                <Route path='*' element={<h1>404 Erro</h1>}/>
             </Routes>
         </BrowserRouter>
     )

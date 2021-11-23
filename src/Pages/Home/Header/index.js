01bb75dc3873da2d19872e85035f25a09fc24f03
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-23 18:50:16 -0300

    Create Header Widget And Header UI

---
+++ src/Pages/Home/Header/index.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react'
+import './style.css'
+
+// Import React Router
+import { useLocation } from 'react-router'
+
+export default function Header(){
+
+    // Get Location
+    const location = useLocation()
+    // Set Src Diretory Images
+    const [srcDiretory, setSrcDirectory] = useState('')
+    useEffect(()=>{
+        if(location.pathname != null){
+            const listSplit = location.pathname.split('/')
+            let x = 0
+            setSrcDirectory('')
+            while(x <= listSplit.length-2){
+                if(x !== listSplit.lenght-2){
+                    setSrcDirectory( s =>  s +"../" )
+                }
+                x++
+            }        
+        }   
+    },[location.pathname])
+
+    return(
+        <header className='header-home'>
+            <img className='logo' src={srcDiretory + 'assets/logo.png'} alt='Logo Icon'/>
+            <div className='menu-input-container'>    
+                <div className='input-area'>
+                    <label><img src={srcDiretory + 'assets/search.png'} alt='Search Icon'/></label>
+                    <input type='text' placeholder='Pesquisar'/>
+                </div>
+                <nav className='menu-area'>
+                    <ul>
+                        <li><button>Categorias</button></li>
+                        <li><button>Promoções</button></li>
+                        <li><button>Histórico</button></li>
+                        <li><button>Vender</button></li>
+                    </ul>
+                </nav>
+            </div>
+            <div className='right-area'>
+                <button>Crie sua conta</button>
+                <button>Entre</button>
+                <button>Compras</button>
+                <img src={srcDiretory + 'assets/carrinho.png'} alt='Carrinho Icon' title='Carrinho de Compras'/>
+            </div>
+        </header>
+    )
+}

99905a815515fecc5ef981c5d88fc41512b4b03e
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-24 18:23:43 -0300

    Add UI

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import './style.css'
 
+// Import React Router
+import { Outlet } from 'react-router'
+
 // Import Widgets
 import Header from './Header'
 
 export default function Home(){
     return(
-        <div>
+        <div className='home-container'>
             <Header/>
+            <div className='outlet-container'>
+               <Outlet/>  
+            </div>
+           
         </div>
     )
 }

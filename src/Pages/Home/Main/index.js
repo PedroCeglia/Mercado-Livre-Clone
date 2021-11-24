c22fb05b170a7cb82317e9937feaf63d3bd7fb7b
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-24 18:20:23 -0300

    Add Home Main Page Widget

---
+++ src/Pages/Home/Main/index.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import './style.css'
+
+// Import Widgets
+import ItemProduto from '../../../Widgets/ItemProduto'
+
+function Main(){
+    return(
+        <div className='main-container'>
+            <div className='list-item-produtos'>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+                <ItemProduto/>
+            </div>
+        </div>
+    )
+}
+
+export default Main

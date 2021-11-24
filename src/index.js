826f7d68eba7a6935c29703a126f8fd8c0ee09b0
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-24 10:49:02 -0300

    Add React-Redux Provider Widget andStore

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Import React Redux
+import {Provider} from 'react-redux'
+// Import Store
+import store from'./Redux/Store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

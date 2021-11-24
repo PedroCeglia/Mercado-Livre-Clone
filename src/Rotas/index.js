1918f45e98c58c9885973a2c416446836a29ffc0
Pedro Ceglia <ceglia.pedro@gmail.com>
2021-11-24 18:22:50 -0300

    Add Vender and Main Routes in Home Route

--- src/Rotas/index.js
+++ src/Rotas/index.js
@@ -7,14 +7,18 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from '../Pages/Home'
 import Erro from '../Pages/Erro'
 import Log from '../Pages/Log'
-
+import Vender from '../Pages/Home/Vender'
+import Main from '../Pages/Home/Main'
 
 export default function Rotas(){
     return(
         <BrowserRouter>
             <Routes>
                 <Route path='/log' element={<Log/>}/>
-                <Route path='/' element={<Home/>}></Route>
+                <Route path='/' element={<Home/>}>
+                    <Route path='/' element={<Main/>}/>
+                    <Route path='/vender' element={<Vender/>}/>
+                </Route>
                 <Route path='*' element={<Erro/>}/>
             </Routes>
         </BrowserRouter>
